40eb3dfc5243a22fdb39e9e8760d328ac9841b20
emrahselek <furkanbasar17@gmail.com>
2021-10-20 11:25:29 -0400

    testNG-framework

---
+++ src/test/java/com/techproed/pages/DefaultPage.java
@@ -0,0 +1,17 @@
+package com.techproed.pages;
+import com.techproed.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class DefaultPage {
+    public DefaultPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//span[@class='hidden-480']")
+    public WebElement addUserButton;
+    @FindBy(xpath = "//span[.='Hotel Management']")
+    public WebElement hotelManagementTab;
+    @FindBy(partialLinkText = "Hotel Rooms")
+    public WebElement hotelRoomsTab;
+}
---
+++ src/test/java/com/techproed/tests/Day12_TestAddressLoginTest.java
@@ -0,0 +1,23 @@
+package com.techproed.tests;
+import com.techproed.pages.TestAddressLoginPage;
+import com.techproed.utilities.ConfigReader;
+import com.techproed.utilities.Driver;
+import org.testng.annotations.Test;
+public class Day12_TestAddressLoginTest {
+    @Test
+    public void testAddressLogin(){
+        //Going to the application page
+        Driver.getDriver().get(ConfigReader.getProperty("test_address_url"));
+        //Creating page object
+        TestAddressLoginPage testAddressLoginPage=new TestAddressLoginPage();
+        //Signing in
+        testAddressLoginPage.email.sendKeys(ConfigReader.getProperty("test_address_email"));
+        testAddressLoginPage.password.sendKeys(ConfigReader.getProperty("test_address_password"));
+        testAddressLoginPage.singInButton.click();
+        //closing the driver
+        Driver.closeDriver();
+
+        //There 100 test case and you used pass word in all 100 tests
+        //and password changes
+    }
+}
